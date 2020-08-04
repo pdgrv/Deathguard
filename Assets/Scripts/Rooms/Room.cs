40dab5c3b7c0526bf6d4f3dd9fc31b2c468b716a
Maxim Podgorov <mapodgorov@gmail.com>
2020-08-04 16:01:56 +0700

    Add room placer

---
+++ Assets/Scripts/Rooms/Room.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public GameObject DoorU;
+    public GameObject DoorR;
+    public GameObject DoorD;
+    public GameObject DoorL;
+}
