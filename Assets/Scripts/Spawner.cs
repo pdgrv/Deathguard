965deab764c0397092a880256e59d0f91957f19f
Maxim Podgorov <mapodgorov@gmail.com>
2020-08-14 20:23:45 +0700

    Add cemetery asset and env

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private List<Wave> _waves;
+    [SerializeField] private List<Transform> _spawnPoints;
+    [SerializeField] private Player _player;
+
+    private Wave _currentWave;
+    private int _currentWaveNumber = 0;
+    private float _timeAfterLastSpawn;
+    private int _spawned;
+
+    private void Start()
+    {
+        SetWave(_currentWaveNumber);
+    }
+
+    private void SetWave(int index)
+    {
+        _currentWave = _waves[index];
+    } 
+}
+
+[System.Serializable]
+public class Wave
+{
+    public GameObject Template;
+    public float Delay;
+    public int Count;
+}
