92209287008c17d55618109384ee9e5ff58576a7
Maxim Podgorov <mapodgorov@gmail.com>
2020-08-29 15:38:48 +0700

    Add main menu

---
+++ Assets/Scripts/UI/HideButton.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideButton : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _menuList;
+
+
+}
