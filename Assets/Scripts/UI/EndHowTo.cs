209a7e330271f8236dc28213f0fea7b639ddff4d
Maxim Podgorov <mapodgorov@gmail.com>
2020-08-29 21:15:02 +0700

    Add HowTo

---
+++ Assets/Scripts/UI/EndHowTo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndHowTo : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            gameObject.SetActive(false); 
+        }
+    }
+}
